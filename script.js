6a741579d40dd368c5be0b458bc50ae9eec8332f
Joe <johansen949@gmail.com>
2022-10-18 11:27:07 +0200

    Push to sync

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 /* VARIABLES */
+let operators = document.getElementsByClassName("operators");
 let buttons = document.getElementsByClassName("btn");
 let buttonsArray = [].slice.call(buttons);
+let operatorsArray = [].slice.call(operators);
 
 let displayScreen = document.getElementById("display-screen");
 let displayText = document.getElementById("display-text");
+let operatorBtn = document.getElementById("operate");
 
 let AC = document.getElementById("btn-AC");
 let C = document.getElementById("btn-C");
+
+
+let tmp1 = "";
+let tmp2 = "";
+let operator = "";
 
 
 /*Iterates through the buttons and add updateDisplay function */
 buttonsArray.forEach(function(element){
     element.addEventListener("click", updateDisplay)
 });
+
+
+/* Adds operate function to = button */
+operatorBtn.addEventListener("click", operate)
 
 
 /*Add's onclick to AC and C button to clear screen and memory */
@@ -22,11 +34,31 @@ C.addEventListener("click", clearMemory);
 
 /* Functions below */
 function updateDisplay(e){
-    let displayValue = displayText.innerHTML 
-    displayValue += e.composedPath()[0].innerHTML;
 
+    //Adds the clicked button to the display field.
+    displayText.innerHTML += e.composedPath()[0].innerHTML;
+
+    //Checks if the entered button is an operator and adds the operator to a variable.
+    if(displayText.innerHTML.slice(-1) == ("+") || displayText.innerHTML.slice(-1) ==("-") 
+    ||displayText.innerHTML.slice(-1) == ("*") || displayText.innerHTML.slice(-1) ==("/")){
+        operator = displayText.innerHTML.slice(-1)
+    }
+
+
+    //Too fix here
+    if(operator == ""){
+        tmp1 += e.composedPath()[0].innerHTML;
+    }else {
+        tmp2 = e.composedPath()[0].innerHTML;
+    }
+
+    if(    displayText.innerHTML.slice(-1) == "="){
+        operate(tmp1,tmp2, operator)
+    }
+    
 }
 
+//Clears the display screen
 function clearScreen(){
     displayText.innerHTML = ""
 }
@@ -36,8 +68,9 @@ function clearMemory(){
 
 }
 
-
+//Arithmatic operations below
 function add (a,b) {
+    
     return a+b;
 }
 
@@ -56,22 +89,19 @@ function divide (a,b) {
     return a/b;
 }
 
-function operate (a,b,c) {
-
-    if (a=="+"){
-
-        return add(b,c);
+function operate (tmp1,tmp2,operator) {
+    if (operator=="+"){
+        displayText.innerHTML += add(tmp1,tmp2);
+        console.log("a")
     }
-    if (a=="-"){
-        return subtract(b,c);
+    if (operator=="-"){
+        return subtract(tmp1,tmp2);
     }
-    if (a=="multiply"){
-        return multiply(b,c);
+    if (operator=="multiply"){
+        return multiply(tmp1,tmp2);
     }
-    if (a=="+"){
-        return divide(b,c);
+    if (operator=="+"){
+        return divide(tmp1,tmp2);
     }
-
 }
 
-operate("+",2,3);
